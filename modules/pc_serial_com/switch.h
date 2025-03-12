b0609789397bd89f7654d38b0dd41df21aec29e8
KostasTzimpilis <tzimpilk@union.edu>
2025-03-12 15:17:24 -0600

    Update and rename pc_serial_com.h to switch.h

---
+++ modules/pc_serial_com/switch.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SWITCH_H_
+#define _SWITCH_H_
+
+//=====[Declaration of public defines]=========================================
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void switchInit();
+
+bool isDoorClosed();
+
+bool isDoorOpen();
+
+//=====[#include guards - end]=================================================
+
+#endif // _SWITCH_H_
