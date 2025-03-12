83c3e60543d5d1ce6430a972181420fc1d989e0c
KostasTzimpilis <tzimpilk@union.edu>
2025-03-12 15:13:02 -0600

    Update and rename intruder_alarm.cpp to siren.cpp

--- modules/intruder_alarm/siren.cpp
+++ modules/intruder_alarm/siren.cpp
@@ -1,56 +1,81 @@
 //=====[Libraries]=============================================================
 
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-#include "intruder_alarm.h"
-#include "motion_sensor.h"
+
+#include "siren.h"
+#include "microwave_system.h"
+#include "switch.h"
+
+
 
 //=====[Declaration of private defines]========================================
 
+
 //=====[Declaration of private data types]=====================================
+
+
+#define SYSTEM_TIME_INCREMENT_MS 10; // need to figure this out
+
 
 //=====[Declaration and initialization of public global objects]===============
 
+
+DigitalOut sirenPin(PE_10);
+
+
 //=====[Declaration of external public global variables]=======================
+
 
 //=====[Declaration and initialization of public global variables]=============
 
+
 //=====[Declaration and initialization of private global variables]============
 
-static bool intruderDetected = OFF;
-static bool intruderDetectorState = OFF;
+
+static bool sirenState = OFF;
+
 
 //=====[Declarations (prototypes) of private functions]========================
 
+
 //=====[Implementations of public functions]===================================
 
-void intruderAlarmInit()
+
+void sirenInit()
 {
-    motionSensorInit();
+    sirenPin = ON;
 }
 
-void intruderAlarmUpdate()
-{
-    intruderDetectorState = motionSensorRead();
 
-    if ( intruderDetectorState ) {
-        intruderDetected = ON;
+bool sirenStateRead()
+{
+    return sirenState;
+}
+
+
+void sirenStateWrite( bool state )
+{
+    sirenState = state;
+}
+
+
+void sirenUpdate( int strobeTime )
+{
+    static int accumulatedTimeAlarm = 0;
+    accumulatedTimeAlarm = accumulatedTimeAlarm + SYSTEM_TIME_INCREMENT_MS;
+   
+    if(cookingDone = true && isDoorClosed) {
+        sirenPin = OFF;
+    }
+    else {
+        sirenPin = ON;
     }
 }
 
-bool intruderDetectorStateRead()
-{
-    return intruderDetectorState;
-}
 
-bool intruderDetectedRead()
-{
-    return intruderDetected;
-}
 
-void intruderAlarmDeactivate()
-{
-    intruderDetected = OFF;
-}
+
 //=====[Implementations of private functions]==================================
