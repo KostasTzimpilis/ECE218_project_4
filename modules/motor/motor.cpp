897dee434f419755c5b48cc82836a8d8ab55ccea
KostasTzimpilis <tzimpilk@union.edu>
2025-03-12 15:15:41 -0600

    Update motor.cpp

--- modules/motor/motor.cpp
+++ modules/motor/motor.cpp
@@ -1,103 +1,104 @@
 //=====[Libraries]=============================================================
 
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+
 #include "motor.h"
+
 
 //=====[Declaration of private defines]========================================
 
+
 #define MOTOR_UPDATE_TIME 9
+
 
 //=====[Declaration of private data types]=====================================
 
+
 //=====[Declaration and initialization of public global objects]===============
+
 
 DigitalInOut motorM1Pin(PF_2);
 DigitalInOut motorM2Pin(PE_3);
 
+
 //=====[Declaration of external public global variables]=======================
+
 
 //=====[Declaration and initialization of public global variables]=============
 
-motorDirection_t motorDirection;
-motorDirection_t motorState;
+
+microwaveDirection_t microwaveDirection;
+microwaveDirection_t microwaveState;
+
 
 //=====[Declaration and initialization of private global variables]============
 
+
 //=====[Declarations (prototypes) of private functions]========================
 
+
 //=====[Implementations of public functions]===================================
+
 
 void motorControlInit()
 {
     motorM1Pin.mode(OpenDrain);
     motorM2Pin.mode(OpenDrain);
-    
+   
     motorM1Pin.input();
     motorM2Pin.input();
 
-    motorDirection = STOPPED;
-    motorState = STOPPED;
+
+   
+    microwaveState = PAUSED;
 }
 
-motorDirection_t motorDirectionRead()
+
+microwaveDirection_t motorDirectionRead()
 {
-    return motorDirection;
+    return microwaveDirection;
 }
 
-void motorDirectionWrite( motorDirection_t direction )
+
+void motorDirectionWrite( microwaveDirection_t direction )
 {
-    motorDirection = direction;
+    microwaveDirection = direction;
 }
+
 
 void motorControlUpdate()
 {
     static int motorUpdateCounter = 0;
-    
+   
     motorUpdateCounter++;
-    
+   
     if ( motorUpdateCounter > MOTOR_UPDATE_TIME ) {
-        
+       
         motorUpdateCounter = 0;
-        
-        switch ( motorState ) {
-            case DIRECTION_1:
-                if ( motorDirection == DIRECTION_2 || 
-                     motorDirection == STOPPED ) {
+       
+        switch ( microwaveState ) {
+            case COOKING:
+                if ( microwaveDirection == PAUSED ) {
                     motorM1Pin.input();
                     motorM2Pin.input();
-                    motorState = STOPPED;
+                    microwaveState = PAUSED;
                 }
             break;
-    
-            case DIRECTION_2:
-                if ( motorDirection == DIRECTION_1 || 
-                     motorDirection == STOPPED ) {
-                    motorM1Pin.input();
-                    motorM2Pin.input();
-                    motorState = STOPPED;
-                }
-            break;
-    
-            case STOPPED:
+   
+            case PAUSED:
             default:
-                if ( motorDirection == DIRECTION_1 ) {
+                if ( microwaveDirection == COOKING ) {
                     motorM2Pin.input();
                     motorM1Pin.output();
-                    motorM1Pin = LOW;
-                    motorState = DIRECTION_1;
-                }
-                
-                if ( motorDirection == DIRECTION_2 ) {
-                    motorM1Pin.input();
-                    motorM2Pin.output();
-                    motorM2Pin = LOW;
-                    motorState = DIRECTION_2;
-                }
+                    microwaveState = COOKING;
+                }    
             break;
         }
-    }        
+    }              
 }
+
 
 //=====[Implementations of private functions]==================================
